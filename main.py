cabb588fee4476bc181c25505f6c75d195819da2
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-01-23 21:44:23 +0000

    Added Vertex AI and Google Auth dependencies. homepath generates 10 animal facts

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+
+import google.auth
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+_, project = google.auth.default()
 
 app = Flask(__name__)
 
-# This code responds to http requests with our HelloWorld greeting
 @app.route("/")
 def hello_world():
-    return f"HelloWorld from main py (homepath route)"
-
+    vertexai.init(project=project, location="us-central1")
+    model = GenerativeModel("gemini-1.5-flash")
+    animal = request.args.get("animal", "parrot") 
+    prompt = f"Give me 10 fun facts about {animal}. Return this as html without backticks."
+    response = model.generate_content(prompt)
+    return response.text
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
